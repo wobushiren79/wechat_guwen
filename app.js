6f8ebabc8ff421eed8c615295f03bc0062bb5344
dqr <517330921@qq.com>
2017-03-27 10:13:59 +0800

    init

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+App({
+    onLaunch: function () {
+        console.log('App Launch')
+    },
+    onShow: function () {
+        console.log('App Show')
+    },
+    onHide: function () {
+        console.log('App Hide')
+    },
+    globalData: {
+        hasLogin: false
+    }
+});
 //app.js
 App({
   onLaunch: function () {
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,8 +3,10 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    grade: '7',
+    service: "5",
+    grade_icon:"../../images/grade.png",
+    service_icon:"../../images/service.png"
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,7 +17,7 @@ Page({
   onLoad: function () {
     console.log('onLoad')
     var that = this
-    //调用应用实例的方法获取全局数据
+  	//调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
