409d311a999931a1fc557eb1952056276c1e6e1e
lenovo <lenovo@LENOVOL-0M2RG22>
2017-04-05 20:06:29 +0800

    姚財武

--- pages/order/order.js
+++ pages/order/order.js
@@ -184,7 +184,14 @@ Page({
                 var categorydata = [];
                 if (category) {
                   for (var i in category) {
-                    DataCategory_b.push(category[i])
+                    if(category[i].count == 0){
+                        category[i].count = 1
+                        DataCategory_b.push(category[i])
+                    }else{
+                        DataCategory_b.push(category[i])
+                    }
+                    
+                    
                   };
                   that.setData({
                     //businessType_e: e.detail.value,
@@ -202,12 +209,13 @@ Page({
   Botton:function(e){
     var id= e.target.dataset.id
     var Cdata=this.data.DataCategory
+    //console.log(Cdata)
    // var DataCategory=[]
     for(var i in Cdata){
         if(Cdata[i].id == id){
-          if(Cdata[i].count !=0){
+          if(Cdata[i].count !=0 && Cdata[i].count !=1){
              Cdata[i].count = Cdata[i].count-1
-             Cdata[i].price = Cdata[i].price - Cdata[i].price
+             Cdata[i].price = Cdata[i].price
              Cdata[i].specification = Cdata[i].specification
              Cdata[i].unit = Cdata[i].unit
              Cdata[i].id = Cdata[i].id
@@ -233,7 +241,7 @@ Page({
     for(var i in Cdata){
         if(Cdata[i].id == id){
              Cdata[i].count = Cdata[i].count + 1
-             Cdata[i].price = Cdata[i].price + Cdata[i].price
+             Cdata[i].price = Cdata[i].price 
              Cdata[i].specification = Cdata[i].specification
              Cdata[i].unit = Cdata[i].unit
              Cdata[i].id = Cdata[i].id
@@ -243,6 +251,9 @@ Page({
       this.setData({
          DataCategory:Cdata
       })
+  },
+  del:function(e){
+    console.log(e.target.dataset.id)
   },
   onLoad: function (options) {
     var that = this
@@ -388,44 +399,44 @@ Page({
   },
   formSubmit: function (e) {
     var ContentData = e.detail.value;
-    if (this.data.businessType == 0) {
-      ContentData.businessType = 1
-    } else if (this.data.businessType == 1) {
-      ContentData.businessType = 2
-    }
-    // 取出緩存登錄信息
-    wx.getStorage({
-      key: 'logindata',
-      success: function (msg) {
-        // console.log(ContentData)
-        var forData = { content: ContentData };
-        //转换字符串
-        var ForData = JSON.stringify(forData);
-        wx.request({
-          url: 'http://115.28.163.211:7080/shianlife-adviser-1.0-SNAPSHOT/setmeal/main/get',
-          method: "POST",
-          data: ForData,
-          header: {
-            "Content-Type": "application/x-www-form-urlencodeed",
-            "Cookie": "sid=" + msg.data.content.sessionId
-          },
-          success: function (res) {
-            //console.log(res.data)
-            if (res.data.code == 1000 && res.data.message == '操作成功') {
-              //console.log(res.data.content.consultId)
-              //操作成功返回consultId進行緩存
-              wx.setStorageSync('consultId', res.data.content.consultId);
-              //頁面跳轉
-              // wx.navigateTo({
-              //      url: '',
-              //  })
-              // console.log(res.data)
-            } else {
-              console.log(res.data.message)
-            }
-          }
-        })
-      }
-    })
+  //   if (this.data.businessType == 0) {
+  //     ContentData.businessType = 1
+  //   } else if (this.data.businessType == 1) {
+  //     ContentData.businessType = 2
+  //   }
+  //   // 取出緩存登錄信息
+  //   wx.getStorage({
+  //     key: 'logindata',
+  //     success: function (msg) {
+  //       // console.log(ContentData)
+  //       var forData = { content: ContentData };
+  //       //转换字符串
+  //       var ForData = JSON.stringify(forData);
+  //       wx.request({
+  //         url: 'http://115.28.163.211:7080/shianlife-adviser-1.0-SNAPSHOT/setmeal/main/get',
+  //         method: "POST",
+  //         data: ForData,
+  //         header: {
+  //           "Content-Type": "application/x-www-form-urlencodeed",
+  //           "Cookie": "sid=" + msg.data.content.sessionId
+  //         },
+  //         success: function (res) {
+  //           //console.log(res.data)
+  //           if (res.data.code == 1000 && res.data.message == '操作成功') {
+  //             //console.log(res.data.content.consultId)
+  //             //操作成功返回consultId進行緩存
+  //             wx.setStorageSync('consultId', res.data.content.consultId);
+  //             //頁面跳轉
+  //             // wx.navigateTo({
+  //             //      url: '',
+  //             //  })
+  //             // console.log(res.data)
+  //           } else {
+  //             console.log(res.data.message)
+  //           }
+  //         }
+  //       })
+  //     }
+  //   })
   }
 });
