d5eb754518985803e8628d87be1c6da3b6a51a99
newapple <734719526@qq.com>
2018-01-22 10:47:33 +0800

    finalfinalfinal修改

--- pages/platform/wallet_apply/wallet_apply.js
+++ pages/platform/wallet_apply/wallet_apply.js
@@ -1,4 +1,4 @@
-var RequestForPlatformm=require('../../../utils/http/RequestForPlatform.js');
+var RequestForPlatformm = require('../../../utils/http/RequestForPlatform.js');
 var RequestForCemetery = require('../../../utils/http/RequestForCemetery.js');
 var toastUtil = require("../../../utils/ToastUtil.js");
 var storageKey = require("../../../utils/storage/StorageKey.js");
@@ -11,7 +11,7 @@ Page({
     value3: '',
     value4: ''
   },
-  money:function(e){
+  money: function (e) {
     // console.log(e.detail.value)
     this.setData({
       money: e.detail.value
@@ -22,32 +22,30 @@ Page({
   },
   onLoad: function () {
     var that = this
-    var getCallBack={
-      success: function (opt,res){
-              var defaultBankCard = opt.defaultBankCard
-              if (defaultBankCard){
-                //取前四位 defaultBankCard.cardNo.substring(0, 4) + '****' + 
-              defaultBankCard.cardNo = defaultBankCard.cardNo.substring(defaultBankCard.cardNo.length - 4)
-              var usableMoney = opt.usableMoney / 100
-              that.setData({
-                usableMoney: getApp().ProcessingPrice(usableMoney),
-                mobile: opt.mobile,
-                defaultBankCard: defaultBankCard,
-                bankCardId: defaultBankCard.id
-              })
-              }else{
-                var usableMoney = opt.usableMoney / 100
-                that.setData({
-                  usableMoney: getApp().ProcessingPrice(usableMoney),
-                  mobile: opt.mobile,
-                  defaultBankCard: defaultBankCard,
-                  // bankCardId: defaultBankCard.id
-                })
-              }
-       },
-       fail:function(){
-         toastUtil.showToast("获取银行卡失败");
-       }
+    var getCallBack = {
+      success: function (opt, res) {
+        var defaultBankCard = opt.defaultBankCard
+        if (defaultBankCard) {
+          defaultBankCard.cardNo = defaultBankCard.cardNo.substring(defaultBankCard.cardNo.length - 4)
+          var usableMoney = opt.usableMoney / 100
+          that.setData({
+            usableMoney: getApp().ProcessingPrice(usableMoney),
+            mobile: opt.mobile,
+            defaultBankCard: defaultBankCard,
+            bankCardId: defaultBankCard.id
+          })
+        } else {
+          var usableMoney = opt.usableMoney / 100
+          that.setData({
+            usableMoney: getApp().ProcessingPrice(usableMoney),
+            mobile: opt.mobile,
+            defaultBankCard: defaultBankCard,
+          })
+        }
+      },
+      fail: function () {
+        toastUtil.showToast("获取银行卡失败");
+      }
     }
     RequestForPlatformm.getCashingInit(null, getCallBack);
   },
@@ -56,27 +54,23 @@ Page({
     var getRequest = {
       mobile: that.data.mobile
     }
-    var getCallBack={
-      success:function(res,opt){
+    var getCallBack = {
+      success: function (res, opt) {
         var Code = opt.data.message
-        var star = Code.indexOf(':')+1;
+        var star = Code.indexOf(':') + 1;
         var co = Code.substring(star)
         console.log(co)
-            that.setData({
-              selected: true,
-              selected1: false,
-              xianshi: true,
-              second: 60,
-              msgCode: co
-            })
-            countdown(that)
-            wx.showToast({
-              title: '发送成功',
-              duration: 3000,
-              // mask:true
-            })
+        that.setData({
+          selected: true,
+          selected1: false,
+          xianshi: true,
+          second: 60,
+          msgCode: co
+        })
+        countdown(that)
+        toastUtil.showToastReWrite('发送成功');
       },
-      fail:function(){
+      fail: function () {
         toastUtil.showToast("发送失败");
       }
     }
@@ -87,93 +81,54 @@ Page({
     var platform = getApp().globalData.platform
     var msgCode = that.data.msgCode
     var get_data = e.detail.value
-    if (get_data.getMoney != '') {
-      if (getApp().AmountVerification(get_data.getMoney)) {
-      if (get_data.getMoney <= that.data.usableMoney){
-      get_data.bankCardId = that.data.bankCardId
-      if (get_data.msgCode == msgCode) {
-        if (get_data.getMoney != '' || get_data.getMoney.length > 0) {
-          if (get_data.bankCardId != undefined || get_data.bankCardId != undefined) {
-            get_data.cashMoney = parseFloat(get_data.getMoney) * 100
-            var  getRequest= get_data
-            var getCallBack={
-              success:function(opt){
-                      that.setData({
-                        disabled:true
-                      })
-                      toastUtil.showToastReWrite("申请成功");
-                      setTimeout(function () {
-                        wx.navigateBack({
-                          delta: 1
-                        })
-                      }, 3000)
-              },
-              fail:function(){
-                toastUtil.showToast("申请失败");
-              }
-            }
-            RequestForPlatformm.getApplyCash(getRequest,getCallBack);
-          } else {
-            wx.hideLoading()
-            wx.showToast({
-              title: '银行卡为空',
-              image: '/images/icon_info.png',
-              duration: 3000,
-            })
-          }
-        } else {
-          wx.hideLoading()
-          wx.showToast({
-            title: '金额为空',
-            image: '/images/icon_info.png',
-            duration: 3000,
-            // mask:true
-          })
-        }
-      } else {
-        wx.hideLoading()
-        wx.showToast({
-          title: '验证码错误',
-          image: '/images/icon_info.png',
-          duration: 3000,
-          // mask:true
+    var submitPrice = parseFloat(get_data.getMoney);
+    var totalPrice = parseFloat(that.data.usableMoney);
+    if (get_data.getMoney == '') {
+      toastUtil.showToast("金额为空");
+      return
+    }
+    if (!getApp().AmountVerification(get_data.getMoney)) {
+      toastUtil.showToast("金额错误");
+      return
+    }
+    if (submitPrice > totalPrice) {
+      toastUtil.showToast("金额过大");
+      return
+    }
+    get_data.bankCardId = that.data.bankCardId
+    if (get_data.msgCode != msgCode) {
+      toastUtil.showToast("验证码错误");
+      return
+    }
+    if (get_data.bankCardId == undefined || get_data.bankCardId == null) {
+      toastUtil.showToast("未选银行卡");
+      return
+    }
+    get_data.cashMoney = submitPrice * 100
+    var getRequest = get_data
+    var getCallBack = {
+      success: function (opt) {
+        that.setData({
+          disabled: true
         })
-      }
-        } else {
-          wx.hideLoading()
-          wx.showToast({
-            title: '金额过大',
-            image: '/images/icon_info.png',
-            duration: 3000,
-            // mask:true
+        toastUtil.showToastReWrite("申请成功");
+        setTimeout(function () {
+          wx.navigateBack({
+            delta: 1
           })
-        }
-    }else{
-      wx.hideLoading()
-      wx.showToast({
-        title: '金额错误',
-        image: '/images/icon_info.png',
-        duration: 3000,
-        // mask:true
-      })
+        }, 3000)
+      },
+      fail: function () {
+        toastUtil.showToast("申请失败");
+      }
     }
-    } else {
-      wx.hideLoading()
-      wx.showToast({
-        title: '金额为空',
-        image: '/images/icon_info.png',
-        duration: 3000,
-        // mask:true
-      })
-    }
+    RequestForPlatformm.getApplyCash(getRequest, getCallBack);
   }
 })
 
 function countdown(that) {
-  // console.log(that.data.second);
   var second = that.data.second;
   if (second == 0) {
-    // console.log("Time Out...");
     that.setData({
       selected: false,
       selected1: true,
@@ -186,6 +141,5 @@ function countdown(that) {
       second: second - 1
     });
     countdown(that);
-  }
-    , 1000)
+  }, 1000)
 }
